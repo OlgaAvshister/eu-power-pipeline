083199182a6342d1b6b0380620184db290d55c3b
Olga Avshister <olya.avshister@yandex.ru>
2026-08-28 16:32:13 +0400

    feat: add deduplication model keeping latest loaded version

---
+++ dbt_power/models/staging/stg_public_power_deduped.sql
@@ -0,0 +1,17 @@
+with ranked as (
+    select
+        *,
+        row_number() over (
+            partition by country, ts_utc, production_type
+            order by loaded_at desc
+        ) as rn
+    from {{ ref('stg_public_power') }}
+)
+select
+    country,
+    ts_utc,
+    production_type,
+    value,
+    loaded_at
+from ranked
+where rn = 1
